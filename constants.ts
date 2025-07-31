80054e13a8e9901e76fb16fe785bdf4c5cfca790
ShebelezA <shimmy.sigasa@gmail.com>
2025-07-31 09:30:47 +0200

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 
-export const GEMINI_MODEL_NAME = "gemini-2.5-flash-preview-04-17";
+export const GEMINI_MODEL_NAME = "gemini-2.5-flash";
 export const GEMINI_IMAGE_MODEL_NAME = "imagen-3.0-generate-002"; // Although not used in this app
 
 export const TEMPLATE_OPTIONS = [
